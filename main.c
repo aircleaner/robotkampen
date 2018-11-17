23d23197f87692a0575e878dc76df99d3771cdc8
Jonas Olofsson <jonas@olofsson.me>
2018-11-17 12:24:39 +0100

    Straight function impl, turn impl

--- main.c
+++ main.c
@@ -1,51 +1,100 @@
 #define LEFT_MOTOR motorB
 #define RIGHT_MOTOR motorA
 #define SYNC_MOTOR synchAB
-#define DIRECTION -1
+#define FORWARD -1
 #define WATER_MOTOR motorC
 #define INIT_POWER 30
+#define TICK500MM 240
+#define TICK100MM 48
+#define TICK90DEG 234
 
-
-//The powers we give to both motors. masterPower will remain constant while slavePower will change so that
-//the right wheel keeps the same speed as the left wheel.
-int masterPower = 0;
-int slavePower = 0;
-//Essentially the difference between the master encoder and the slave encoder. Negative if slave has
-//to slow down, positive if it has to speed up. If the motors moved at exactly the same speed, this
-//value would be 0.
-int error = 0;
-//'Constant of proportionality' which the error is divided by. Usually this is a number between 1 and 0 the
-//error is multiplied by, but we cannot use floating point numbers. Basically, it lets us choose how much
-//the difference in encoder values effects the final power change to the motor.
-int kp = 5;
+#pragma DebuggerWindows("nxtLCDScreen")
 
 int path[100];
 
 
-int driveForward(short degrees)
+void driveStraightDistance(int centimeter, int masterPower)
 {
-  nSyncedMotors = SYNC_MOTOR;
+	nSyncedMotors = synchNone;
+  int tickGoal = centimeter * TICK100MM;
+
+  //This will count up the total encoder ticks despite the fact that the encoders are constantly reset.
+  int totalTicks = 0;
+
+  //Initialise slavePower as masterPower - 5 so we don't get huge error for the first few iterations. The
+  //-5 value is based off a rough guess of how much the motors are different, which prevents the robot from
+  //veering off course at the start of the function.
+  int slavePower = masterPower - 0;
+
+  int error = 0;
+
+  int kp = 1;
+
+  SensorValue[LEFT_MOTOR] = 0;
+  SensorValue[RIGHT_MOTOR] = 0;
+
+  //Monitor 'totalTicks', instead of the values of the encoders which are constantly reset.
+  while(abs(totalTicks) < tickGoal)
+  {
+    //Proportional algorithm to keep the robot going straight.
+    motor[LEFT_MOTOR] = masterPower;
+    motor[RIGHT_MOTOR] = slavePower;
+
+    error = SensorValue[LEFT_MOTOR] - SensorValue[RIGHT_MOTOR];
+
+    slavePower += error / kp;
+
+    SensorValue[LEFT_MOTOR] = 0;
+    SensorValue[RIGHT_MOTOR] = 0;
+
+    wait1Msec(100);
+
+    //Add this iteration's encoder values to totalTicks.
+    totalTicks+= SensorValue[LEFT_MOTOR];
+  }
+  motor[LEFT_MOTOR] = 0; // Stop the loop once the encoders have counted up the correct number of encoder ticks.
+  motor[RIGHT_MOTOR] = 0;
+}
+
+int driveForward(int ticks)
+{
+	nSyncedMotors = SYNC_MOTOR;
   nSyncedTurnRatio = +100;              // move in a straight line
 
-  nMotorEncoder[motorB] = 0;
-	nMotorEncoderTarget[RIGHT_MOTOR] = 360;
+  nMotorEncoder[RIGHT_MOTOR] = 0;
+	nMotorEncoderTarget[RIGHT_MOTOR] = ticks;
 
-  motor[RIGHT_MOTOR] = INIT_POWER * DIRECTION;
+  motor[RIGHT_MOTOR] = INIT_POWER * FORWARD;
+  //motor[LEFT_MOTOR] = INIT_POWER * FORWARD;
 
 	while(nMotorRunState[RIGHT_MOTOR] != runStateIdle)  // while Motor B is still running:
 	{
   	// Do nothing
 	}
 	motor[RIGHT_MOTOR] = 0;
-	return nMotorEncoder[motorB];
+	//motor[LEFT_MOTOR] = 0;
+	return nMotorEncoder[RIGHT_MOTOR];
 }
 
-void turnRight()
+void turn45Degree(int turnDirection)
 {
-	// TODO
+	nSyncedMotors = SYNC_MOTOR;
+	// Rotate in place to turn robot
+	nSyncedTurnRatio = -100;              // rotate in place
+	nMotorEncoderTarget[RIGHT_MOTOR] = TICK90DEG;    // move 200 encoder counts and stop
+	motor[RIGHT_MOTOR] = 50 * turnDirection;
+
+	while(nMotorRunState[RIGHT_MOTOR] != runStateIdle)  // while Motor B is still running:
+	{
+  	// Do nothing
+	}
+	motor[RIGHT_MOTOR] = 0;
 }
 
 task main()
 {
-  driveForward(360);
+	//nSyncedMotors = synchNone;
+  //nSyncedMotors = SYNC_MOTOR;
+  driveStraightDistance(1000, INIT_POWER * FORWARD);
+  //turn45Degree(1);
 }
